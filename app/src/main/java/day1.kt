720989a6df94ce72f6bed1a4b8b0f4e3a7eef762
Carlos <carlos_arce93@hotmail.com>
2021-06-30 21:19:17 -0500

    solve day1

---
+++ app/src/main/java/day1.kt
@@ -0,0 +1,16 @@
+import java.io.*
+import java.util.*
+
+fun main(args: Array<String>) {
+    val i = 4
+    val d: Double = 4.0
+    val s = "HackerRank "
+
+    val i2: Int  = readLine()!!.toInt()
+    val d2: Double = readLine()!!.toDouble()
+    val s2: String? = readLine()
+
+    println(i+i2)
+    println(d+d2)
+    println(s+s2)
+}
